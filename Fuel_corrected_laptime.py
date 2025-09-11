2f13f8bc78ed02b20f14f2063bd1000763ab09dd
Martin Minh-Tien TRAN <61981402+minhtien0207@users.noreply.github.com>
2025-09-11 19:39:33 +0200

    Update Fuel_corrected_laptime.py

--- Fuel_corrected_laptime.py
+++ Fuel_corrected_laptime.py
@@ -1,4 +1,7 @@
+import streamlit as st
+
 import fastf1 as ff1
+#from fastf1.ergast import Ergast
 from fastf1 import plotting
 from fastf1 import utils
 
@@ -12,14 +15,24 @@ import datetime as dt
 import numpy as np
 import pandas as pd
 
-#An attempt to normalize race time by fuel consumption correction
-#This is the based formula:
-#Laptime (FC) = Laptime - (total lap of the GP - lap n°) * Fuel consumption rate * Weight effect
-#This is based on the assumption that 1kg of fuel slow the car down by 0.03s (First assumption)
-#The fuel consumption rate should be calculate by the maximum fuel in use (maximum 110kg, with 5kg in reserve at the end, so 105kg can be use) (Second assumption)
-#Divide these 105kg of fuel by the number of laps of that GP will give the fuel rate consumption.
+#Streamlit display components
+st.header("Formula 1 Fuel-corrected race pace analysis")
 
-#Prepare a dataframe of all the GP.
+st.text("In motor racing, towards the end of the race, cars get lighter as fuel is consumed, so how should we compare these laps?\n")
+st.text("This approach is called fuel correction, which renders different lap times set by cars more comparable.\n")
+st.text("However, fuel consumption data per car per lap is logically unavailable for us fans, so assumptions have to be made.\n")
+st.text("The first assumption is that 1kg of fuel slows the car down by 0.03s, based on this reddit thread: \n")
+st.text("https://www.reddit.com/r/F1Technical/comments/11oskuy/computation_of_fuelcorrected_lap_time \n")
+st.text("The second assumption is how to calculate the fuel consumption rate: \n")
+st.text("The current F1's regulation allows each car to hold 110kg of fuel, and a minimum of 5kg of reserve at the end for random check, thus 105kg to be used.\n")
+st.text("With these two assumptions, our fuel corrected laptime can be calculate as follows: \nLaptime (FC) = Laptime - (total lap of the GP - lap n°) * Fuel consumption rate * Weight effect \n")
+st.text("The fuel consumption rate = 105 kg of fuel / the length of the race track, thus different for each track.\n")
+st.caption("Disclaimer: Argument can be made this approach to be called 'adjusted based on assumption' rather than 'fuel corrected'.\n")
+
+st.text("Choose the year, the circuit and the session to analyze")
+st.text("The option of Sprint Qualifying appears but will only obviously works for event with a sprint shootout session")
+
+
 schedule = ff1.get_event_schedule(2025)
 schedule['EventName']
 
@@ -57,3 +70,4 @@ ax.set_xlabel("Lap Number")
 ax.set_ylabel("Lap Time(s)")
 ax.set_title("Leclerc Bahrain 2024 GP race pace")
 ax.legend()
+
